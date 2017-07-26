0c42204405c12fbbbbf53f07b1eac4169a4c7e2f
PerseiX <kamilrytel2@gmail.com>
2017-07-26 20:09:12 +0200

    Some changes

--- src/SortAndFilterBundle/Controller/DefaultController.php
+++ src/SortAndFilterBundle/Controller/DefaultController.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace SortAndFilterBundle\Controller;
 
@@ -7,11 +8,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
-        return $this->render('SortAndFilterBundle:Default:index.html.twig');
-    }
+	/**
+	 * @Route("/")
+	 */
+	public function indexAction()
+	{
+		return $this->render('SortAndFilterBundle:Default:index.html.twig');
+	}
 }
